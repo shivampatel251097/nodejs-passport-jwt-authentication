7a0f2b5addb2ca4e845e2a709dda798eadc39528
Shivam Patel <shivampatel251097@gmail.com>
2021-01-21 03:26:25 +0530

    assignmentt 4

--- authenticate.js
+++ authenticate.js
@@ -38,3 +38,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
 }));
 
 exports.verifyUser  = passport.authenticate('jwt',{session: false});
+
+exports.verifyAdmin = (req,res,next) =>{
+    if(req.user.admin){
+        next();
+    }
+    else{
+        var err =new Error('You are not authorized!');
+        err.statusCode = 403;
+        return next(err);
+    }
+}
